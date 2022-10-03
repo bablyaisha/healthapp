d691b35153b526fb3772d8808b30cb827b8bc65c
bablyaisha <bablyaisha786@gmail.com>
2022-10-03 20:18:50 +0530

    nav done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import Navbar from './components/navbar';
+import Home from './pages/home';
 import {BrowserRouter as Router} from 'react-router-dom';
 
 function App() {
   return (
     <Router>
-     <Navbar />
+     <Home />
     </Router>
   );
 }
